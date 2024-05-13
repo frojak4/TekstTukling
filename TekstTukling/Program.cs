24da154ea6fbffe0e161598d15eef0311bc7c452
frojak4 <Frojak4@gmail.com>
2024-05-13 10:08:05 +0200

    Initial commit

---
+++ TekstTukling/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace TekstTukling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            string userInput = "";
+            while (userInput is not "1" and not "2") {
+                Console.WriteLine("Velkommen til TekstTukling!");
+                Console.WriteLine("Kva vil du gjere?");
+                Console.WriteLine("1. Roter tekst");
+                Console.WriteLine("2. Skift ord");
+
+                userInput = Console.ReadLine();
+            }
+
+            if (userInput == "1")
+            {
+                rotateText();
+            } else if (userInput == "2")
+            {
+                changeWord();
+            }
+        }
+
+        static void rotateText()
+        {
+            Console.WriteLine("Korleis tekst vil du rotere?");
+            string userInput = Console.ReadLine();
+            string rotatedWord = "";
+            
+            for (int i = userInput.Length; i > 0; i--)
+            {
+                
+                rotatedWord += userInput[i - 1];
+                
+            }
+
+            Console.WriteLine($"{rotatedWord} er teksten ditt rotert!");
+        }
+
+        static void changeWord()
+        {
+            Console.WriteLine("Korleis tekst vil du skifte?");
+            string userInput = Console.ReadLine();
+            
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == 'e')
+                {
+                    StringBuilder sb = new StringBuilder(userInput);
+                    sb[i] = 'a';
+                    userInput = sb.ToString();
+                }
+            }
+
+            Console.WriteLine($"{userInput} er din nye tekst");
+        }
+    }
+}
